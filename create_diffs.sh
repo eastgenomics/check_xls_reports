53e55f1ee0960e5ee0e0fc7f84885a82aadbea1e
Yu-jinKim <yujin.kim@hotmail.fr>
2020-09-22 13:25:24 +0100

    script to generate the diffs programmatically

--- create_diffs.sh
+++ create_diffs.sh
@@ -1,12 +1,14 @@
 for new in $(ls new/*csv); do
     new_name=$(echo $new | cut -d/ -f2)
+    sample_name=$(echo $new | cut -d/ -f2 | cut -d_ -f1)
 
     for old in $(ls old/*csv); do
         old_name=$(echo $old | cut -d/ -f2)
 
         if [ "$new_name" = "$old_name" ]; then
             suffix=$(echo $old_name | cut -d_ -f3-)
-            diff $new $old > $suffix
+            echo $sample_name"_"$suffix
+            diff $new $old > diffs/$sample_name"_"$suffix
         fi
     done
 done
